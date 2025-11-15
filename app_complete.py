5370f00742408e16b05f0f90c2eecec6f4e94422
nawazshaik313 <nawazshaikmohammed313@gmail.com>
2025-11-15 07:23:31 +0530

    updated requirement

--- app_complete.py
+++ app_complete.py
@@ -7,7 +7,6 @@ import razorpay
 from email.message import EmailMessage
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from datetime import datetime
